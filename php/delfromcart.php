6bdba17021b21813ee37303459031ddcd35ed4c0
mandarin41 <tyugai96@gmail.com>
2017-05-30 11:04:37 +0700

    kek

---
+++ php/delfromcart.php
@@ -0,0 +1,21 @@
+<?php session_start();
+for ($i=0; $i<$_SESSION['count']; $i++)
+{
+	$itemnumber='';
+	$itemnumber=$_POST['name'.$i];
+	if ($itemnumber!='')
+	{
+		$itemnumber=$i;
+		break;
+	}
+}
+for ($i=$itemnumber; $i<$_SESSION['count']; $i++)
+{
+	$next=$i+1;
+	$_SESSION['name'.$i]=$_SESSION['name'.$next];
+	$_SESSION['price'.$i]=$_SESSION['price'.$next];
+	$_SESSION['numb'.$i]=$_SESSION['numb'.$next];
+}
+$_SESSION['count']--;
+echo '<script>location.href="', $_SERVER['HTTP_REFERER'], '"</script>';
+?>
